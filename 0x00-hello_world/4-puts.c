9ae5805edaae92eb805535f6a069c65cef163a6d
Afnan Ahmed <i_afnan@icloud.com>
2023-08-10 10:05:00 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: 'the program's description'
- * @parameter: describe the parameter
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
