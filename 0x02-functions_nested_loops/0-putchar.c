f8e47f7d4494bd6adf10ad4f07028d2094670a7c
Afnan Ahmed <i_afnan@icloud.com>
2023-08-15 04:02:33 -0700

    New program to print array

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char put[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(put[i]);
+		_putchar('\n);
+	}
+	return (0);
+}
