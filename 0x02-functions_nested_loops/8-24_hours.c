851ff40ca4a35a815d337ba9f976b5a4f63d51a6
Afnan Ahmed <i_afnan@icloud.com>
2023-08-15 13:10:25 -0700

    Add clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * jack_bauer - a function to count the day's hourse and minutes.
+ *
+ *
+ * Return: hourse and minutes
+ */
+void jack_bauer(void)
+{
+	char i = '0', j, k, n;
+
+	while (i < '3')
+	{
+		j = '0';	
+		while (j <= '9')
+		{
+			k = '0';
+			while (k < '6')
+			{
+				n = '0';
+				while (n <= '9')  
+				{
+					_putchar(i);
+					if (i == 2)
+					{
+						_putchar('3');
+					}
+					else
+					{
+						_putchar(j);
+					}
+					_putchar(':');
+					_putchar(k);
+					_putchar(n);
+					_putchar('\n');
+					n++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
