446404335fcc0f9f042a0302834bea93ff4f0722
Afnan Ahmed <i_afnan@icloud.com>
2023-08-15 05:46:36 -0700

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function to be called
+ *
+ * Return: print alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int alpha;
+	int i = 0;
+
+	while (i < 11)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
