87f38fba06af6058158cc4198929801b3fccd78d
Afnan Ahmed <i_afnan@icloud.com>
2023-08-15 09:53:16 -0700

    Detect lowercase value

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - a funtion to detect lower/upper case character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
