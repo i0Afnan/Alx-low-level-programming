a7620d20f94448607008e00a6191866a9ecee9e7
Afnan Ahmed <i_afnan@icloud.com>
2023-08-17 12:24:20 -0700

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,11 @@
  */
 void more_numbers(void)
 {
-	int n, i = 1, j = 0;
+	int n, i, j;
 
-	for (; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			n = j;
 			if (j > 9)
