3b40a91f63cbb518a2a0b4e10c936f6940ea606d
Afnan Ahmed <i_afnan@icloud.com>
2023-08-17 12:13:27 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print to 14
+ *
+ * Return: number to 14
+ */
+void more_numbers(void)
+{
+	int n, i = 1, j = 0;
+	
+	for (; i <= 10; i++)
+	{
+		for (; j <= 14; j++)
+		{
+			n = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				n = j % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
