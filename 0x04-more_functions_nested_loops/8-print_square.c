51ad33066b46862e476c192aab42da9286a6f631
Afnan Ahmed <i_afnan@icloud.com>
2023-08-17 17:11:26 -0700

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints # mark
+ *
+ * @size: size of square
+ *
+ * Return: draw sqaure
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
