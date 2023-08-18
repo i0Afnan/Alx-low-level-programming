488ac681aa5ab0ad366d16729a76870d3acd8059
Afnan Ahmed <i_afnan@icloud.com>
2023-08-17 17:42:56 -0700

    Print triangle '

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: a triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if ((i + j) <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
