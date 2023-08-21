46357a1cb6f4113068336c55235270c5c6f59e84
Afnan Ahmed <i_afnan@icloud.com>
2023-08-21 09:47:32 -0700

    Reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes value of an intended var to 98
+ *
+ * @*n: the var to be reset
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
