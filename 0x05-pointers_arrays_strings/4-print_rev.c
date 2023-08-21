717c2c4ad749d468f1398f8d6f8f8aa24cef03f5
Afnan Ahmed <i_afnan@icloud.com>
2023-08-21 15:44:38 -0700

    Print reversed text

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @str: string to be printed
+ *
+ * Return: nothing
+ */
+void print_rev(char *str)
+{
+	int i;
+	
+	i = _strlen(str);
+
+	for (; i >= 0; i--)
+	{
+	_putchar(str[i]);
+	}
+	 _putchar('\n');
+}
