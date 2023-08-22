6baf67a52edb444419b621083c216a1e2a0954e0
Afnan Ahmed <i_afnan@icloud.com>
2023-08-21 17:17:31 -0700

    Print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 - print every other character of a string
+ *
+ * @str: string to be printed
+ *
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int size, i;
+
+	size = _strlen(str);
+
+	for (i = 0; i < size - 1 ; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
