695001046de08333959e4935ab108ef1d0194adb
Afnan Ahmed <i_afnan@icloud.com>
2023-08-24 10:02:57 -0700

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * *_strncpy - a function that copies a string
+ * _strncpy - a function that copies a string
  *
  * @dest: string to copy to
  *
  * @src: source of string
  *
  * @n: index
+ *
+ * Return: coppied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -15,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (; src[i] && i < n; i++)
 		dest[i] = src[i];
-	
+
 	while (i < n)
 	{
 		dest[i] = '\0';
