6eb169cd3980d5b08ea7e77701a809d895447f0a
Afnan Ahmed <i_afnan@icloud.com>
2023-08-24 19:34:23 -0700

    Leet:

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Converts letters into leet
+ *
+ * @l: string which letter is converted
+ *
+ * Return: encoded string
+ */
+char *leet(char *l)
+{
+	char *from = "aAeEoOtTlL";
+	char *to = "4433007711";
+	int i = 0, j;
+
+	while (l[i])
+	{
+		for (j = 0; from[j]; j++)
+		{
+			if ( l[i] == from[j])
+				l[i] = to[j];
+
+		}
+		i++;
+	}
+	return (l);
+}
