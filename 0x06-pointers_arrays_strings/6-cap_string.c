1b19b16c36c64e9df745810c794d412c0a6d8efd
Afnan Ahmed <i_afnan@icloud.com>
2023-08-24 17:54:44 -0700

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all word of a string
+ *
+ * @s: string
+ *
+ * Return: capitalized first letter of each word
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(',
+			')', '{', '}'};
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+		if (s[i - 1] == sep[j] && s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+		}
+	}
+	return (s);
+}
