2f14ada793d373dcb29344a100565dacf6255ab2
Afnan Ahmed <i_afnan@icloud.com>
2023-08-24 18:59:45 -0700

    Changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * lower - check if the letter is in lowercase
+ *
+ * @l: letter 
+ *
+ * Return: 1 if letter is lowercase and 0 otherwise
+ */
+int lower(int l)
+{
+
+	return (l >= 97 && l <= 122);
+
+}
+
+/**
+ * sep - detects seperator
+ *
+ * @d: char to detect
+ *
+ * Return: 1 for sep 0 for none
+ */
+int sep(char p)
+{
+	return (p == ',' || p == ';' || p == '.' || p == '!' || p == '?' || 
+			p == '\"' || p == '(' || p == ')' || p == '{' || 
+			p == '}' || p == ' ' || p == '\n' || p == '\t');
+}
+
+/**
  * *cap_string - capitalize all word of a string
  *
  * @s: string
@@ -9,26 +37,11 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(',
-	')', '{', '}'};
+	int i;
 
-	for (; s[i] != '\0'; i++)
-	{
-		for (j = 0; sep[j]; j++)
-		{
-			if (s[i - 1] == sep[j])
-			{
-				if (s[i] >= 97 && s[i] <= 122)
-				{
-					s[i] -= 32;
-				}
-				else
-					continue;
-			}
-			else
-				continue;
-		}
-	}
+	for (i = 0; s[i]; ++i)
+		if (i == 0 || sep(s[i - 1]))
+			(lower(s[i])) ? s[i] -= 32 : 0;
+
 	return (s);
 }
