0ed082c6c188dfcb8fd15a4f971f27492cf560da
Afnan Ahmed <i_afnan@icloud.com>
2023-08-24 18:41:11 -0700

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,14 +13,17 @@ char *cap_string(char *s)
 	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(',
 	')', '{', '}'};
 
-	for (; s[i]; ++i)
+	for (; s[i] != '\0'; i++)
 	{
 		for (j = 0; sep[j]; j++)
 		{
-			if (s[i - 1] == sep[j] && s[i] >= 97 && s[i] <= 122)
-			{
-			s[i] -= 32;
-			}	
+			if (s[i - 1] == sep[j])
+				if (s[i] >= 97 && s[i] <= 122)
+				{
+					s[i] -= 32;
+				}
+				else
+					continue;
 		}
 	}
 	return (s);
