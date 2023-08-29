57e933307c9e32dade487eb7c56632dffda35cf9
Afnan Ahmed <i_afnan@icloud.com>
2023-08-28 17:16:28 -0700

    Print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i = 0, j;
 
-	for (; i < 0; i++)
+	for (; i < 8; i++)
 	{
-		for (j = 0; j < 0; j++)
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 
 	_putchar('\n');
