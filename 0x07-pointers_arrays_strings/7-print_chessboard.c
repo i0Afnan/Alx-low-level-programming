a54b3518deb6fa79d1830540c761aeab03e78b14
Afnan Ahmed <i_afnan@icloud.com>
2023-08-28 17:10:26 -0700

    Make chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function to print chessboard
+ *
+ * @a: array
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	for (; i < 0; i++)
+	{
+		for (j = 0; j < 0; j++)
+			_putchar(a[i][j]);
+
+	_putchar('\n');
+	}
+}
