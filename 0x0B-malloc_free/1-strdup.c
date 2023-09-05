dfe30dce6b5d0b11e056f56b98438dc787b2e1d6
Afnan Ahmed <i_afnan@icloud.com>
2023-09-05 04:02:24 -0700

    Copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - Returns a copy of a string
+ *
+ * @str: string to be copied
+ *
+ * Return: a pointer on success, NULL otherwise
+ */
+char *_strdup(char *str)
+{
+	int size = 0, i = 0;
+	char *dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; str[size] != '\0'; size++)
+	;	
+
+	dup = malloc(size * sizeof(*str) + 1);
+
+	if (dup == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+		{
+			dup[i] = str[i];
+		}
+	}
+	return (dup);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,6 +6,7 @@
 
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+char *_strdup(char *str);
 
 
 #endif
