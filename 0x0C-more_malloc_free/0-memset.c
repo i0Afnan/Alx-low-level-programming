5d77b1dafd903bbc6a005c92df5d5eb9d7a24b6a
Afnan Ahmed <i_afnan@icloud.com>
2023-09-08 13:05:53 -0700

    allocate memory for an array

---
+++ 0x0C-more_malloc_free/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - a function to fill memory with constant byte.
+ *
+ * @s: pointer
+ *
+ * @b: constant byte
+ *
+ * @n: number of bytes
+ *
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+	int i = 0;
+
+	for (; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "0-memset.c"
+
+/**
+ * *_calloc - allocates memory for an array
+ *
+ * @nmemb: array
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	p = malloc(sizeof(int) * nmemb);
+
+	if (p == 0)
+		return (NULL);
+
+	_memset(p, 0, sizeof(int) * nmemb);
+
+	return (p);
+}
