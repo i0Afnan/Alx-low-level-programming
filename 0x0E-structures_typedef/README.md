06fa50f8b3da2c5d5730e4009c62851d8306d6e1
Afnan Ahmed <i_afnan@icloud.com>
2023-09-12 16:46:38 -0700

    build struct for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# 0x0E. C - Structures, typedef
+This project is an implementation of learning `strict`.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for doggy
+ *
+ * @name: dog's name
+ * @age: doggy's age
+ * @owner: the owner of the dog
+ *
+ * Description: dog's general information
+ */
+
+struct dog
+{
+	char *name, owner;
+	float age;
+}
