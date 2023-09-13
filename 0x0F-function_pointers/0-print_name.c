4024ab3c529cefedb6e243a583e30878129d80ad
Afnan Ahmed <i_afnan@icloud.com>
2023-09-13 16:25:17 -0700

    Build pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name: name
+ * @f: function to be called
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUN_POINTERS
+#define FUN_POINTERS
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif
