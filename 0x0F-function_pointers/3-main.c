b0658263daf9c6ddd85e2ef72f8f17fdffe2ffe9
Afnan Ahmed <i_afnan@icloud.com>
2023-09-13 17:38:14 -0700

    Build operation functions using pointers to functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - start point
+ *
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+	int a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!op_func)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%d\n", func(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition operation
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substraction operation
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_dev - division operation
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulation operation
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
