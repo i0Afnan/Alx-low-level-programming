2ef4eee06965e0644c2dbfc79f38d2fa68f2369e
Afnan Ahmed <i_afnan@icloud.com>
2023-09-21 14:31:12 -0700

    Print elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * _strlen - a function to count the length of a string
+ *
+ * @s: a variable of type string
+ *
+ * Return: The length of a stringi, 0 if null
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+
+/**
+ * print_list - a function that prints all the elements of a list.
+ *
+ * @h: head
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		printf("[%d] %s\n", _strlen(h->str), h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+# 0x12. C - Singly linked lists
+
+This project is a practice to learn Linked list concept.
+Following questions were concedered and answered during solving the tasks:
+- When and why using linked lists vs arrays?
+- How to build and use linked lists?
