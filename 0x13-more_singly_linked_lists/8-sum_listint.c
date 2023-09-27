0df7d814d4142ead9f03365ebaea5a1c5578afc1
Afnan Ahmed <i_afnan@icloud.com>
2023-09-26 19:07:13 -0700

    Print sum of all of the data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of a linked list
+ *
+ * @head: the head of the list
+ *
+ * Return: sum of all the data (n) of a listint_t linked list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	for (sum = 0; head; head = head->next)
+		sum += head->n;
+
+	return (sum);
+}
