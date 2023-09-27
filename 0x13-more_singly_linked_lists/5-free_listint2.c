f7a08cd1ccb13c957cadfdec21ea85f0fd4d4ec2
Afnan Ahmed <i_afnan@icloud.com>
2023-09-26 19:01:40 -0700

    free list while setting the head NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ *
+ * @head: head of the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
